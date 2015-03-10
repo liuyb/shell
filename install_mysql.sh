30b09a931f0c1493d9a01a80fa824f3a5b39dd54
liuyb <liuyb@me.com>
2015-03-10 23:09:34 +0800

    install gcc gcc-c++ cmake... before install mysql

--- install_mysql.sh
+++ install_mysql.sh
@@ -6,8 +6,16 @@ if [ $(id -u) != "0" ]; then
     exit;
 fi
 
+#install cmake
+yum install gcc gcc-c++ ncurses-devel cmake
+
+#get mysql
 wget http://dev.mysql.com/get/Downloads/MySQL-5.6/mysql-5.6.23.tar.gz
+
 tar -zxvf mysql-5.6.23.tar.gz
 cd mysql-5.6.23
+cmake . -DCMAKE_INSTALL_PREFIX=/usr/local/services/mysql -DSYSCONFDIR=/usr/local/services/mysql/etc -DMYSQL_DATADIR=/data/mysql -DMYSQL_UNIX_ADDR=/data/mysql/mysqld.sock -DWITH_INNOBASE_STORAGE_ENGINE=1 -DENABLED_LOCAL_INFILE=1 -DMYSQL_TCP_PORT=3306 -DEXTRA_CHARSETS=all -DDEFAULT_CHARSET=utf8 -DDEFAULT_COLLATION=utf8_general_ci -DMYSQL_UNIX_ADDR=/data/mysql/mysql.sock -DWITH_DEBUG=0
+make
+make install
 
 exit 0
