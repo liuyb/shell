e530c4d34470c66a31f56f00275621e73c19f312
liuyb <liuyb@me.com>
2015-03-14 14:24:35 +0800

    update how to install php

--- install_php.sh
+++ install_php.sh
@@ -5,3 +5,13 @@ if [ $(id -u) != "0" ]; then
     echo "error: user must be an administrator"
     exit;
 fi
+
+#wget php
+wget http://am1.php.net/distributions/php-5.6.6.tar.gz
+tar -zxvf php-5.6.6.tar.gz
+cd php-5.6.6
+./configure --prefix=/usr/local/services/php --enable-fpm --with-config-file-path=/usr/local/services/php/etc --enable-mbstring --disable-mbregex --enable-sockets --disable-pdo --disable-phar --with-sqlite3 --with-mcrypt --with-gd --with-jpeg-dir= --with-freetype-dir  --with-curl --enable-soap --with-zlib --with-libxml-dir --enable-zip --with-xmlrpc --enable-pcntl --with-mysql=/usr/local/services/mysql --enable-mysqlnd --with-mysqli=mysqlnd --with-openssl
+make
+make install
+
+exit 0
